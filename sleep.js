a3cb5b9a1e61c93c665994c4c3187a3ba9b1bc10
u1pns <46481880+u1pns@users.noreply.github.com>
2024-12-01 20:48:24 +0100

    Update sleep.js
    New strategy (only for windows), self adjusting Sleep
    Old code commented

--- sleep.js
+++ sleep.js
@@ -1,6 +1,19 @@
 //
-// Sleep function for nodejs sync needs using different strategies
+// Self adjustable WINDOWS Sleep function for nodejs sync 
 //
+// If you use NODEJS for batch processes in windows, and you a tired on falling in the await/asycn nightmare, maybe this will help....
+
+var Sleep = (function(i){
+  var iSleepAdjust = 50;
+  return function(i){
+    let stime = performance.now();  
+    require('child_process').execSync("echo WSH.Sleep("+(i-iSleepAdjust)+"); > ~$timer.tmp & cscript //e:jscript /Nologo ~$timer.tmp & del ~$timer.tmp");
+    if (performance.now()-stime>=i) iSleepAdjust+=1;
+    else iSleepAdjust-=1;
+  }})();
+
+/* OLD VERSION
+//  Sleep function for nodejs sync needs using different strategies
 function Sleep(i)
 {
     if (i>=1000) {
@@ -15,3 +28,5 @@ function Sleep(i)
         while (performance.now() - stime < i) require('child_process').execSync("ping 127.0.0.1 -n 1 > nul");
     }
 }
+
+*/
